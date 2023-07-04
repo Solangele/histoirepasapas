58586ff4b2e3a16ce430adaaf52d23f4cdcbd737
unknown <angedespretz@gmail.com>
2023-07-04 16:33:57 +0200

    hey

--- cartehistorique.js
+++ cartehistorique.js
@@ -4,6 +4,19 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
-  .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+L.marker([49.04, 3.40]).addTo(map)
+  .bindPopup('Musée Jean de la Fontaine.')
   .openPopup();
+
+  L.marker([49.02, 3.44]).addTo(map)
+  .bindPopup('Ferme Historique Jean de la Fontaine.')
+  .openPopup();
+
+  L.marker([48.86, 2.39]).addTo(map)
+  .bindPopup('Cimetière du Père-Lachaise.')
+  .openPopup();
+
+  L.marker([48.86, 2.33]).addTo(map)
+  .bindPopup('Musée du Louvre.')
+  .openPopup();
+
